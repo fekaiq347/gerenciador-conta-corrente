ff8f6073273e520868c1a1b605bcb485d47a1c0b
fekaiq <kaiquefe@dcc.ufrj.br>
2025-06-06 12:02:38 -0300

    Revert "Seeds corrigido."
    This reverts commit 389639ee22f7a10109f17ebb828236e4b0e1f444.

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,7 +5,7 @@
 c = Correntista.find_or_initialize_by(conta_numero: "12345")
   c.nome      = "Cliente Normal"
   c.senha     = "1010"
-  c.perfil    = "normal"
+  c.perfil    = "NORMAL"
 
   c.build_conta_corrente(
     saldo: 1000.00,
@@ -18,7 +18,7 @@ c = Correntista.find_or_initialize_by(conta_numero: "12345")
 c = Correntista.find_or_initialize_by(conta_numero: "54321")
   c.nome      = "Cliente VIP"
   c.senha     = "0101"
-  c.perfil    = "vip"
+  c.perfil    = "VIP"
   
   c.build_conta_corrente(
     saldo: 5000.00,
